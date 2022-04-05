e4ccc98281bd714618e7683341baf2dd131fb0df
unknown <mevinc.71@gmail.com>
2022-04-05 01:45:17 -0700

    fixed css

--- Movie-App/script.js
+++ Movie-App/script.js
@@ -4,55 +4,29 @@ const SEARCHAPI="https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee9
 
 
 
-const main= document.querySelector("main");
-const form= document.querySelector("form");
-const search=document.querySelector("search");
+const main= document.getElementById("main");
+const form= document.getElementById("form");
+const search=document.getElementById("search");
 
 //to get favorite movies
-getMovies();
+getMovies(APIURL);
 
 
-async function getMovies(){
-    const resp= await fetch(APIURL);
+async function getMovies(url){
+    const resp= await fetch(url);
     const respData=await resp.json();  
-
+     
     console.log(respData);
 
-    // respData.results.forEach(movie => {
-    //     const img = document.createElement("img");
-    //     img.src = IMGPATH + movie.poster_path;
+    showMovies(respData.results);
 
-    //     document.body.appendChild(img);
-    // });
-
-    respData.results.forEach(movie=>{
-        const {poster_path,title,vote_average}=movie;
-
-        const movieEl=document.createElement("div");
-        movieEl.classList.add("movie");
-
-        movieEl.innerHTML=`
-        <img 
-        src="${IMGPATH + movie.poster_path}"
-        alt="${title}"                          
-        />
-        <div class="movie-info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
-        `;
-
-        main.appendChild(movieEl);
-    });
-
-    return respData;
 }
 function showMovies(movies){
 
     main.innerHTML="";
     
-    movies.results.forEach(movie=>{
-        const {poster_path,title,vote_average}=movie;
+    movies.forEach((movie)=>{
+        const {poster_path,title,vote_average,overview }=movie;
 
         const movieEl=document.createElement("div");
         movieEl.classList.add("movie");
@@ -66,12 +40,15 @@ function showMovies(movies){
         <h3>${title}</h3>
         <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
+        <div class="overview">
+        <h3>Overview:</h3>
+        ${overview}
+        </div>
         `;
 
         main.appendChild(movieEl);
     });
 }
-
 
 
 function getClassByRate(vote){
@@ -88,4 +65,14 @@ form.addEventListener("submit",(e)=>{
     e.preventDefault();
 
     const searchTerm=search.value;
+
+    if (searchTerm) {
+
+        getMovies(SEARCHAPI + searchTerm);
+
+
+    search.value="";
+
+    }
+
 });
